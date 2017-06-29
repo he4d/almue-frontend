7b779ae260d80e5e07651caa1da5f8b277bc86b8
Tobias Tschinkowitz <he4d@posteo.de>
2017-06-29 18:21:58 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero">
+    <div class="hero-head">
+      <app-header></app-header>
+    </div>
+    <div class="hero-body">
+      <router-view></router-view>
+    </div>
+    <div class="hero-footer">
+    </div>
+  </section>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import "sass/index.scss";
+</style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="nav">
+    <div class="container">
+      <div class="nav-left">
+        <router-link to="/" exact class="nav-item is-brand">
+          <img v-if="home" src="../assets/logo.png" alt="Almue">
+          <img v-else src="../assets/logo.png" alt="Almue">
+        </router-link>
+      </div>
+      <div class="nav-center">
+        <a class="nav-item" href="https://github.com/he4d/almue" target="_blank">
+          <b-icon pack="fa" icon="github"></b-icon>
+        </a>
+      </div>
+
+      <span class="nav-toggle" :class="{ 'is-active': isMenuActive }" @click="isMenuActive = !isMenuActive">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+
+      <div class="nav-right nav-menu" :class="{ 'is-active': isMenuActive }">
+        <router-link to="/" exact class="nav-item">Home</router-link>
+        <router-link to="/control" exact class="nav-item">Control</router-link>
+        <router-link to="/manage" exact class="nav-item">Manage</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isMenuActive: false
+    }
+  }
+}
+</script>
